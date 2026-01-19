da527d29e59db6cf1e4c68046ddc1506d03fd8ae
Alex <alexisloubatiere@gmail.com>
2026-01-19 11:51:32 +0100

    petite mdif monthcontroller

---
+++ backend/src/controllers/monthController.js
@@ -0,0 +1,110 @@
+const monthService = require("../services/monthService");
+
+// Créer un mois
+const createMonth = async(req , res)=> {
+    try {
+        const monthData = req.body;
+        
+        const newMonth = await monthService.createMonth(monthData);
+        res.json({
+            success: true,
+            data: newMonth,
+        });
+
+    } catch (error) {
+        console.error("Erreur lors de la création du mois", error);
+        res.status(500).json({
+            success: false,
+             message: `Erreur serveur ${error.messsage}`,
+        });  
+    }
+};
+
+// Récupérer tous les mois
+const getAllMonths = async(req , res)=> {
+     try {
+        const months = await monthService.getAllMonths();
+
+        res.json({
+            success: true,
+            data: months,
+    });
+    } catch (error) {
+        console.error("Erreur lors de la récupération des mois", error);
+        res.status(500).json({
+            success: false,
+             message: `Erreur serveur ${error.messsage}`,
+        });
+    }
+};
+
+// Récupérer un mois par son ID
+const getMonthById = async(req , res)=> {
+     try {
+        const { id } = req.params;
+
+        const month = await monthService.getMonthById(id);
+
+        res.json({
+            success: true,
+            data: month,
+    });
+   } catch (error) {
+        console.error("Erreur lors de la récupération du mois", error);
+        res.status(500).json({
+            success: false,
+            message: `Erreur serveur ${error.messsage}`,
+        });        
+    }
+};
+
+// Modifier un mois par son ID
+const updateMonthById = async (req, res)=> {
+    try {
+        const { id } = req.params;
+        const monthData = req.body;
+
+        const updatedMonth = await monthService.updateMonthById(id, monthData)
+
+        res.json({
+            success: true,
+            data: updatedMonth,
+        });
+
+    } catch (error) {
+        console.error("Erreur lors de la modification du mois", error);
+        res.status(500).json({
+            success: false,
+             message: `Erreur serveur ${error.messsage}`,
+        }); 
+    }
+};
+
+// Supprimer un mois par son ID
+const deleteMonthById = async(req, res)=> {
+    try {
+        const { id } = req.params;
+
+        const month = await monthService.deleteMonthById(id);
+
+        res.json({
+            success: true,
+            data: month,
+    });
+    } catch (error) {
+        console.error("Erreur lors de la suppression du mois", error);
+        res.status(500).json({
+            success: false,
+             message: `Erreur serveur ${error.messsage}`,
+        });   
+    }
+};
+
+
+module.exports = {
+    createMonth,
+    getAllMonths,
+    getMonthById,
+    updateMonthById,
+    deleteMonthById
+};
