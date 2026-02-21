c37654bf44abdd29415c404ba174dbb28c31c92d
Alex <alexisloubatiere@gmail.com>
2026-02-21 16:53:58 +0100

    test unitaire POST et GET pour month

--- backend/tests/month.test.js
+++ backend/tests/month.test.js
@@ -1,21 +1,23 @@
 const request = require("supertest");
 const sequelize = require("../src/config/database");
 const app = require("../src/app");
-
+const User = require("../src/models/User");
+const jwt = require("jsonwebtoken");
 
 describe("Month Endpoints", () => {
     beforeEach(async ()=> {
         await sequelize.models.Month.destroy({ where: {} });
+        await User.destroy({where: {} });
     });
 
-    // POST/
-    describe("POST /api/months", () => {
-        let token;
-        // Création Utilisateur
-        beforeEach(async () => {
-            await sequelize.models.Month.destroy({ where: {} });
 
-            const res = await request(app)
+    // POST
+    describe("POST /api/months - admin", () => {
+        let token;
+
+        // Création utilisateur nécessaire
+        beforeEach(async () => {
+            await request(app)
                 .post("/api/auth/register")
                 .send({
                     username: "admin",
@@ -23,19 +25,23 @@ describe("Month Endpoints", () => {
                     password: "AdminPassword123",
                     favorite_player: "Jordan",
                 });
-            
-            // Passage Admin
-            const user = await sequelize.models.Users.findOne({
+
+            // Passage utilisateur en Admin
+            const user = await User.findOne({
                 where: { email: "admin@example.com" }
             });
 
             user.role = "admin";
             await user.save();
 
-            token = res.body.data.token;
+            token = jwt.sign(
+                { id: user.id, role: user.role },
+                process.env.JWT_SECRET,
+                { expiresIn: "7d" }
+            );
         });
 
-        // Création d'un mois
+        // Création du mois
         it("Doit créer un nouveau mois", async () => {
             const res = await request(app)
                 .post("/api/months")
@@ -46,14 +52,18 @@ describe("Month Endpoints", () => {
                     end_date: "2026-01-31",
                     publish_date: "2025-12-31",
                     status: "open",
-                });
-            expect(res.statusCode).toBe(201);
-            expect(res.body.success).toBe(true);
-            expect(res.body.data).toHaveProperty("id");
-            expect(res.body.data.label).toBe("testMonth");
-            expect(res.body.data.status).toBe("open");
+            });
+        
+
+        expect(res.statusCode).toBe(201);
+        expect(res.body.success).toBe(true);
+        expect(res.body.data).toHaveProperty("id");
+        expect(res.body.data.label).toBe("testMonth");
+        expect(res.body.data.status).toBe("open");
         });
 
+
+        // Test erreur création si champs manquant
         it("Ne peut pas créer de mois car champs manquant", async () => {
             const res = await request(app)
                 .post("/api/months")
@@ -70,9 +80,9 @@ describe("Month Endpoints", () => {
     });
     
 
+    // Test erreur création pour non Admin
     describe("POST /api/months", () => {
         let token;
-        // Test erreur création pour non Admin
         beforeEach(async () => {
             await sequelize.models.Month.destroy({ where: {} });
 
@@ -87,7 +97,6 @@ describe("Month Endpoints", () => {
 
             token = res.body.data.token;
         });
-
         it("Doit refuser la création pour un utilisateur non admin", async () => {
             const res = await request(app)
                 .post("/api/months")
@@ -104,4 +113,64 @@ describe("Month Endpoints", () => {
         });
     });
 
+
+    //  /GET
+    describe("GET /api/month/", () => {
+         // Création de mois pour le test
+        beforeEach(async () => {
+            await request(app)
+                .post("/api/auth/register")
+                .send({
+                    username: "admin",
+                    email: "admin@example.com",
+                    password: "AdminPassword123",
+                    favorite_player: "Jordan",
+                });
+
+            // Passage utilisateur en Admin
+            const user = await User.findOne({
+                where: { email: "admin@example.com" }
+            });
+
+            user.role = "admin";
+            await user.save();
+
+            token = jwt.sign(
+                { id: user.id, role: user.role },
+                process.env.JWT_SECRET,
+                { expiresIn: "7d" }
+            );
+        
+
+            // Création des mois pour le test
+            await sequelize.models.Month.bulkCreate([
+                {
+                    label: "Janvier 2026",
+                    start_date: "2026-01-01",
+                    end_date: "2026-01-31",
+                    publish_date: "2025-12-31",
+                    status: "open",
+                },
+                {
+                    label: "Février 2026",
+                    start_date: "2026-02-01",
+                    end_date: "2026-02-28",
+                    publish_date: "2026-01-31",
+                    status: "open",
+                }
+            ]);
+        });
+
+        // Récupération des mois existants
+        it("Doit récupérer la liste des mois", async () => {
+            const res = await request(app)
+                .get("/api/months")
+                .set("Authorization", `Bearer ${token}`);
+
+            expect(res.statusCode).toBe(200);
+            expect(res.body.success).toBe(true);
+            expect(Array.isArray(res.body.data)).toBe(true);
+            expect(res.body.data.length).toBe(2);
+        });
+    });
 });
