a5d9b9366af926849329b221b597291c0ba4e17f
Alex <alexisloubatiere@gmail.com>
2026-03-11 11:00:14 +0100

    mise en place frontend dossier + framwork + dependecies + context (authprovider)

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import AuthProvider from './context/AuthProviders';
+
+function App() {
+
+return (
+    <AuthProvider>
+        {/* Routes ici */}
+    </AuthProvider>
+)
+}
+
+export default App
---
+++ frontend/src/context/AuthProviders.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { useEffect, useState, createContext } from "react";
+import api from "../services/api";
+
+const AuthContext = createContext();
+
+// Mettre a disposition de tous les composants de l'application tous les données pour mettre a jour et recuperer les données de l'utilisateur connecté
+export default function AuthProvider({ children }) {
+    const [user, setUser] = useState(null);
+    const [token, setToken] = useState(localStorage.getItem("token") || null);
+    const [loading, setLoading] = useState(true);
+  
+    useEffect(() => {
+        async function fetchMe() {
+        if (!token) {
+            setLoading(false);
+            return;
+        }
+        try {
+            const { data } = await api.get("/auth/me");
+            setUser(data.data);
+        } catch {
+            localStorage.removeItem("token");
+            setToken(null);
+        } finally {
+            setLoading(false);
+        }
+        }
+        fetchMe();
+    }, [token]);
+
+  
+    const login = async (email, password) => {
+        const { data } = await api.post("/auth/login", { email, password });
+        localStorage.setItem("token", data.data.token);
+        setToken(data.data.token);
+        setUser(data.data.user);
+    };
+    const register = async (username, email, password, favorite_player = "") => {
+        const { data } = await api.post("/auth/register", {
+            username,
+            email,
+            password,
+            favorite_player,
+        });
+        
+        localStorage.setItem("token", data.data.token);
+        setToken(data.data.token);
+        setUser(data.data.user);
+    };
+    const logout = () => {
+        localStorage.removeItem("token");
+        setToken(null);
+        setUser(null);
+    };
+    return (
+        <AuthContext.Provider value={{ user, token, loading, login, register, logout }}>
+        {children}
+        </AuthContext.Provider>
+    );
+}
